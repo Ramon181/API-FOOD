a02446ae0a4096cab1cabb43736c253d21b6cf5d
Ramon181 <ramonantonio29970476@gmail.com>
2022-08-03 12:17:13 +0200

    ot

--- src/db.js
+++ src/db.js
@@ -11,7 +11,7 @@ const sequelize = new Sequelize(DATABASE_URL, {
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectOptions: {
     ssl: {
-      require: false,
+      require: true,
       rejectUnauthorized: false,
     }
   }
